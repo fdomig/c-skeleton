fa68ae1e5f4a3b93bff6f5ae4759a2e6c9e889b8
Franziskus Domig <fdomig@gmail.com>
2014-09-23 13:28:30 +0200

    Fixed typo

--- tests/minunit.h
+++ tests/minunit.h
@@ -6,9 +6,9 @@
 #include <stdlib.h>
 #include "dbg.h"
 
-#define mu_start_suite() char *message = NULL;
+#define mu_suite_start() char *message = NULL;
 
-#define mu_assert(test, message) if (!(test)) { log_error(message); return message; }
+#define mu_assert(test, message) if (!(test)) { log_err(message); return message; }
 #define mu_run_test(test) debug("\n-----%s", " " #test); \
 	message = test(); tests_run++; if (message) return message;
 
